529a7be2bd2a3dc2cdb3cf19732f842b876c05ce
Miguel Machado <miguelpmachado@users.noreply.github.com>
2018-04-27 15:52:46 +0100

    Update install.sh

--- docs/install.sh
+++ docs/install.sh
@@ -1,10 +1,38 @@
 mkdir oneida_tools/
 cd oneida_tools/
+
+# Aspera Connect
 wget https://download.asperasoft.com/download/sw/connect/3.7.4/aspera-connect-3.7.4.147727-linux-64.tar.gz
 tar -xf aspera-connect-3.7.4.147727-linux-64.tar.gz 
 bash aspera-connect-3.7.4.147727-linux-64.sh
 rm aspera-connect-3.7.4.147727-linux-64.*
 mv /home/participant/.aspera/connect/ ./aspera_connect/
 echo 'export PATH="/home/participant/oneida_tools/aspera_connect/bin:$PATH"' >> ~/.bashrc
+
+# getSeqENA
 git clone https://github.com/B-UMMI/getSeqENA.git
 echo 'export PATH="/home/participant/oneida_tools/getSeqENA:$PATH"' >> ~/.bashrc
+
+# Linuxbrew
+git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+PATH="/home/participant/.linuxbrew/bin:$PATH"
+echo 'export PATH="/home/participant/.linuxbrew/bin:$PATH"' >> ~/.bashrc
+echo 'export MANPATH="$(brew --prefix)/share/man:$MANPATH"' >> ~/.bashrc
+echo 'export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"' >> ~/.bashrc
+
+# mlst
+brew tap brewsci/science
+brew tap brewsci/bio
+brew untap homebrew/science
+brew update
+brew install mlst
+
+# INNUca
+git clone https://github.com/B-UMMI/INNUca.git
+echo 'export PATH="/home/participant/oneida_tools/INNUca:$PATH"' >> ~/.bashrc
+
+# Prokka
+brew install brewsci/science/prokka
+
+# Abricate
+brew install brewsci/science/abricate
